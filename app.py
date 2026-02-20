6b92d37912c222fefbcf915b83061cf1f0708cbb
Marcosggvv <marcosversiane@gmail.com>
2026-02-20 16:37:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ with st.sidebar:
 
                 try:
                     resposta = client.models.generate_content(
-                        model="gemini-1.5-flash",
+                        model="gemini-2.0-flash",
                         contents=prompt,
                     )
                     
@@ -193,7 +193,7 @@ with aba1:
 
             try:
                 resposta = client.models.generate_content(
-                    model="gemini-1.5-flash",
+                    model="gemini-2.0-flash",
                     contents=prompt,
                 )
                 
@@ -286,5 +286,6 @@ with aba3:
     else:
 
         st.info("Inicie a resolução de itens para compilar os dados estatísticos.")
+
 
 
